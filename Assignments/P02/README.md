14f3540f625fa84a67b24b67dd7ae5451746aa6e
Joshua Beaty <joshua.a.beaty@gmail.com>
2022-04-06 06:13:23 -0500

    Reformatted output

--- Assignments/P02/README.md
+++ Assignments/P02/README.md
@@ -6,19 +6,30 @@
 - The linked list should hold `wordNodes`.
 - Time how long it takes to load the data into your linked list (we will use that later as well).
 - After your dictionary (linked list) is loaded, we are going to perform "autosuggestions" when a user types characters at the console.
-- Suggestions will start after 1 character is typed, however only the top 10 suggestions will be printed along with the total number of matching words (example output below).
+- Suggestions will start after 1 character is typed, however only the top 10 suggestions will be printed along with the total number of matching words.
 - Matching suggestions will ONLY be words that match the substring starting from position zero. For example: `cat` would match `cat`, `caterpillar`, and `cattle`. But NOT `wildcat`.
 - As typing, the time it takes to find suggestions will be displayed in seconds.
-- In addition I have included a file to help color text. Docs are here: https://termcolor.readthedocs.io/ but I included some examples in [main.cpp](main.cpp).
+
+#### Input:
+- baz
+
+#### Output:
+- baz
+- 
+- 6 words found in 36.929000 seconds
+- 
+- bazaar bazaars bazon bazoo bazooka bazookas
 
 ### Files
-
 |   #   | File                    | Description                                                 |
 | :---: | ----------------------- | ----------------------------------------------------------- |
 |   1   | [main.cpp](./main.cpp)  | Main driver of the program                                  |
 |   2   | [wordList.hpp](./wordList.hpp)  | HPP holds the stuct for node and class for WordList |
 |   3   | [dictionary](./dictionary.txt)  | Dictionary for word completion        |
+|   4   | [output](./output.txt)  | Output file keeps track of all searches made        |
 
 ### Instructions
-
-- click the links if desired
+- download main.exe and dictionary.txt into the same folder
+- run main.exe
+- pressing a character will create a prefix
+- it will print out the number of words and the first 10 results with that prefix(NOTE: this takes 30+ seconds on my machine.)
--- Assignments/P02/main.cpp
+++ Assignments/P02/main.cpp
@@ -34,19 +34,66 @@ using namespace std;
  */
 WordList FindWords(WordList wordList, string substring) {
     WordList matches; // to hold any matches
+    int count = 0;
+    bool found = false;
     for (int i = 0; i < wordList.size; i++) { // loop through wordList
-        bool found = false;
+        if(found)
+            found = false;
 
         //if the substring matches the corresponding characters of the prefix in wordList found = true
         if(wordList[i].substr(0, substring.size()).compare(substring) == 0)
             found = true;
 
-        if (found) {
+        if (found && count < 10) {
             matches.Push(wordList[i]);   // add to matches
+            count++;
         }
     }
 
     return matches;
+}
+
+/**
+ * Description:
+ *      Prints out the results of the wordList to the console and to the output file.
+ * Params:
+ *      WordList wordList - WordList of matches
+ * 
+ * Returns:
+ *      NULL
+ */
+void PrintResult(WordList wordList, Timer t, char k, string sub, ofstream& fout){
+    string output = "";
+    //limit output to just 10 results
+    int results = wordList.size;
+    if(results > 10)
+        results = 10;
+
+    output += sub;
+    output += "\n\n";
+    output += to_string(wordList.size);
+    output += " words found in ";
+    output += to_string((double)((double)(t.MilliSeconds())/1000));
+    output += " seconds\n\n";  
+    cout << output;
+    fout << output;
+    cout << termcolor::red;
+
+    // This prints out some matches
+    for (int a = 0; a < results; a++) {
+        //substring characters turn red rest are green
+        for(int b = 0; b < wordList[a].size(); b++){
+            if(b >= sub.size())
+                cout << termcolor::green;
+            cout << wordList[a][b];
+            fout << wordList[a][b];
+        }
+        cout << ' ' << termcolor::red;
+        fout << ' ';
+    }
+
+    cout << termcolor::reset << endl;
+    fout << '\n' << endl;
 }
 
 int main() {
@@ -55,9 +102,9 @@ int main() {
     WordList words;                     // wordList of words from dictionary
     WordList matches;                   // any matches found in words
 
-    ofstream fout("temp.txt");          //output file
+    ofstream fout("output.txt");          //output file
     ifstream fin("dictionary.txt");     //input file
-
+    
     Timer T;   // create a timer
     T.Start(); // start it
 
@@ -68,15 +115,12 @@ int main() {
     T.End(); // end the current timer
 
     // print out how long it took to load the dictionary file
-    cout << T.Seconds() << " seconds to read in\n";
-    cout << T.MilliSeconds() << " milliseconds to read in\n";
-    cout << T.NanoSeconds() << " nanoseconds to read in\n";
-
+    cout << to_string((double)((double)T.MilliSeconds()/1000)) << " seconds to read in\n";
     cout << "Type keys and watch what happens. Type capital Z to quit." << endl;
 
     // While capital Z is not typed keep looping
     while ((k = getch()) != 'Z') {
-        T.Start(); // start it
+        system("clear");
         // Tests for a backspace and if pressed deletes
         // last letter from "word".
         // 8 is backspace on my machine.
@@ -98,49 +142,13 @@ int main() {
             }
             word += k; // append char to word
         }
-
-        // Find the prefix in the wordList that matches the substring
-        matches = FindWords(words, word);
-
-        if ((int)k != 32) { // if k is not a space print it
+        // only use resources if we have a substring to search and the most recent char wasn't a space
+        if(word.size() > 0 && (int)k != 32){
+            T.Start();
+            // Find the prefix in the wordList that matches the substring
+            matches = FindWords(words, word);
             T.End();
-            cout << T.Seconds() << " seconds" << endl;
-            cout << "Keypressed: " << termcolor::blue << k << " = " << (int)k << termcolor::reset << endl;
-            cout << "Current Substr: " << termcolor::red << word << termcolor::reset << endl;
-            cout << "Words Found: ";
-            cout << termcolor::red;
-            if(word.size() > 0){
-                // This prints out all found matches
-                for (int a = 0; a < matches.size; a++) {
-                    //substring characters turn red rest are green
-                    for(int b = 0; b < matches[a].size(); b++){
-                        if(b >= word.size())
-                            cout << termcolor::green;
-                        cout << matches[a][b];
-                    }
-                    cout << ' ' << termcolor::red;
-                }
-                cout << termcolor::reset << endl;
-            }
+            PrintResult(matches, T, k, word, fout);
         }
-    }
-    //Output spits out to temp.txt after the search is complete
-    fout << T.Seconds() << " seconds" << endl;
-    fout << "Keypressed: " << termcolor::blue << k << " = " << (int)k << termcolor::reset << endl;
-    fout << "Current Substr: " << termcolor::red << word << termcolor::reset << endl;
-    fout << "Words Found: ";
-    fout << termcolor::red;
-    if(word.size() > 0){
-        // This prints out all found matches
-        for (int a = 0; a < matches.size; a++) {
-            //substring characters turn red rest are green
-            for(int b = 0; b < matches[a].size(); b++){
-                if(b >= word.size())
-                    fout << termcolor::green;
-                fout << matches[a][b];
-            }
-            fout << ' ' << termcolor::red;
-        }
-        cout << termcolor::reset << endl;
     }
 }
